859ce700efd0072d58caa55539638f147645174c
Malena Müller <malemuller@Malenas-MacBook.local>
2019-04-02 11:57:34 -0300

    anda transpose y isSymmetric, me falta ver si funciona cholesky

--- TP1_EJ1_CHOLESKY/cholesky.py
+++ TP1_EJ1_CHOLESKY/cholesky.py
@@ -1,69 +1,95 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 #from scipy.linalg import toeplitz
 
-print("hello world")
+#print("hello world")
 
-def cholesky(a):
-    print(a)
-    print(a[0][1])
-    print(" chau")
-    return a
-
-def isSimetric(a):
-    # A.transpose()
-    print("ADENTRO a PRINCIPIO: ", a)
-    at=a
-    print("ADENTRO a PRINCIPIO 1: ", a)
+def transpose(a):
+    w,h = len(a),len(a[0])
+    at = [[0 for x in range(w)] for y in range(h)]
     # iterate through rows
     for i in range(len(a)):
         # iterate through columns
         for j in range(len(a[0])):
             at[j][i] = a[i][j]
-            j=j+1
-        i=i+1
+            j=j+1               #PUEDE SER QUE ESTO NO HAGA FALTA?
+        i=i+1                   #PUEDE SER QUE ESTO NO HAGA FALTA?
+    return at
 
-    print("ADENTRO a: ", a)
-    print("ADENTRO at: ", at)
+def isSymmetric(a):
+    #print("MATRIZ RECIBIDA: ", a)
+    if len(a) != len(a[0]):
+        return False
+
+    at = transpose(a)
+    print("AL FINAL a: ", a)
+    print("AL FINAL at: ", at)
+
     if at == a:
         return True
     else:
         return False
 
+
+def cholesky(a):
+    if isSymmetric(a):
+        w = len(a)
+        g = [[0 for x in range(w)] for y in range(w)]
+        #-------------------------------
+        for j in range(w):
+            sum = 0
+            for k in range(j - 1):
+                sum += ((g[j][k]) ** 2)
+            g[j][j] = math.sqrt(a[j][j] - sum)
+            j = j + 1
+        #-------------------------------
+            # iterate through rows
+        for i in range(w):
+            # iterate through columns
+            for j in range(i-1):
+                sum = 0
+                for k in range(j-1):
+                    sum += (g[i][k] * g[j][k])
+                g[i][j] = (a[i][j] - sum) / (g[j][j])
+                j = j + 1               #PUEDE SER QUE ESTO NO HAGA FALTA?
+            i = i + 1                   #PUEDE SER QUE ESTO NO HAGA FALTA?
+        gt = transpose(g)
+        matArr = [g, gt]
+        return (matArr)
+    else:
+        return [False, False]
+
 #------------------------------------------------------
-# Program to transpose a matrix using nested loop
 
-X = [[12,7],
-    [4 ,5],
-    [3 ,8]]
-
-result = [[0,0,0],
-         [0,0,0]]
-# iterate through rows
-for i in range(len(X)):
-   # iterate through columns
-   for j in range(len(X[0])):
-       result[j][i] = X[i][j]
-
-for r in result:
-   print(r)
 #------------------------------------------------------
 
-aa=[[2, 3, 2],
-   [3, 5, 3],
-   [2, 3, 3]]
+aa = [[2, 3, 2],
+    [3, 5, 3],
+    [2, 3, 3]]
 
-cc=np.matrix('2 3 2; 3 5 3; 2 3 3')
+#cc=np.matrix('2 3 2; 3 5 3; 2 3 3')
 
-a=[[0, 0, 0],
-   [0, 0, 0],
-   [0, 0, 0]]
+a = [[0, 0, 0],
+    [0, 0, 0]]
 
-bb=[[0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8]]
+b = [[0, 1, 2],
+    [3, 4, 5]]
 
-a=cholesky(aa)
-print("bb=", bb)
-print("ES aa SIMETRICA? ", isSimetric(bb))
+bb = [[0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]]
+
+#a=cholesky(aa)
+#print("bb=", bb)
+print("mat transpuesta: ", transpose(b))
+print("Is matrix simetric?:", isSymmetric(bb))
+gygt = cholesky(aa)
+print ("g= ", gygt[0])
+print ("gt= ", gygt[1])
+g = gygt[0]
+gt = gygt[1]
+print ("g= ", g)
+print ("gt= ", gt)
+
 #isTransposeOf(bb)
