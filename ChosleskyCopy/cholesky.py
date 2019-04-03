3e342362edf485ee7bb4f8d4006f8b67f66ab2e3
jmestanza <jmestanza@itba.edu.ar>
2019-04-03 02:21:58 -0300

    lo mejore un poquito

--- ChosleskyCopy/cholesky.py
+++ ChosleskyCopy/cholesky.py
@@ -3,45 +3,46 @@ import matplotlib.pyplot as plt
 import math
 #from scipy.linalg import toeplitz
 
+
 def isSymmetric(a, tol=1e-8):
     #falta chequear si es de nxn
     return np.allclose(a, a.transpose(), atol=tol)
+
+
 def isEqual(a,b, tol=1e-8):
     return np.allclose(a, b, atol=tol)
 
 
-def cholesky(a):
-    if isSymmetric(a):
-        w = len(a)
-        n = w + 1
-        l = np.zeros((n,n))
-        sum1 = 0
-        sum2= 0
-        #p es i fila
-        #q es j columna
-        for j in range(1, n):  # j es columna
-            for i in range(j, n):  # esto esta bien
-                #esto esta bien
-                #----------------------------
-                if i==j:
-                    for k in range(1,i):
-                        sum1 += (l[i][k]) ** 2
-                    l[i][i] = (a[i - 1][i - 1] - sum1) ** (1 / 2)
-                    sum1 = 0
-                else:
-                    for k in range(1,j):
-                        sum2+=l[i][k]*l[j][k]
-                    l[i][j] = (a[i-1][j-1]-sum2)/(l[j][j])
-                    sum2 = 0
+def is_pos_def(x):
+    return np.all(np.linalg.eigvals(x) > 0)
 
-        #estas borran los 0s sin utilizar para que sea adecuada la matriz
-        ans = l.copy()
-        ans = np.delete(ans,0,1)
-        ans = np.delete(ans,0,0)
-        return ans
+
+def cholesky(a):
+
+    if isSymmetric(a) and is_pos_def(a):
+        w = len(a)
+        l = np.zeros((w,w)) # matriz de wxw
+        #sum1 y sum2 son las sumatorias de las formulas
+        sum1 = 0
+        sum2 = 0
+        #j es columna,i es fila
+        #como lo primero que termina una iteracion en el proceso son las filas
+        #entonces deben estar adentro del for de las columnas
+        for j in range(0, w):
+            for i in range(j, w):
+                if i == j: #formula para cuando i vale j
+                    for k in range(0, i):
+                        sum1 += (l[i][k]) ** 2
+                    l[i][i] = (a[i][i] - sum1) ** (1 / 2)
+                    sum1 = 0
+                else:     #formula para cuando i distinto j
+                    for k in range(0, j):
+                        sum2 += l[i][k]*l[j][k]
+                    l[i][j] = (a[i][j]-sum2)/(l[j][j])
+                    sum2 = 0
+        return l
     else:
         return None
-
 #------------------------------------------------------
 
 j = np.array([[25,15,-5,-10],
