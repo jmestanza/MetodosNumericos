ad3d3acd3a75ea9d2f527d987f4f0038b0c50cce
jmestanza <jmestanza@itba.edu.ar>
2019-04-05 01:05:06 -0300

    despues de años lo hice andar :)

--- EJ1/ej1.py
+++ EJ1/ej1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import toeplitz
-from subs import *
+from utils import *
 L = 5
 ganancia = 1/10
 h = ganancia*(1+np.random.randn(L))
@@ -43,33 +43,46 @@ rE = rE.transpose()
 S = toeplitz(sE.transpose())
 #S = S(:,1:L) # en octave dame todas las columnas de 1 a L
 S= S[:,0:L+1] # en python 0:L+1 incluye hasta L
-print("hola")
 S = np.tril(S)
-print(S)
 # #print(HE.dot(sE))
-# he=solveEq(S,rE)
-# z= np.zeros(M-L)
-# Heaux=np.concatenate((he,z),axis=None)
-# He= toeplitz(Heaux,np.zeros(M))
-# CN= (sigma**2)*np.eye(len(He))
-#
-# sigmax= (1/256*(sum( np.power (((range(0,255))-np.mean(range(0,255))),2))))**(1/2)
-# mx= np.mean(range(0,255))*np.ones(M)
-# CX= (sigmax**2)*np.eye(len(He))
-#
-# W = CX.dot(He.transpose())*np.linalg.inv((He.dot(CX)).dot(He.transpose)+CN)
-#
-# d = np.zeros((M,P))
-# for k in range(1,P+1):
-#     #d(:,k) accede en octave a la columna k-esima
-#     d[:, k-1] = W.dot((r[:, k-1])-He.dot(mx))+ mx
-# d = d.astype(int)
-#
-# plt.figure(2)
-# plt.subplot(1,3,1)
-# plt.imshow(a,cmap='gray',vmin=1,vmax=255)
-# plt.subplot(1,3,2)
-# plt.imshow(b,cmap='gray',vmin=1,vmax=255)
-# plt.subplot(1,3,2)
-# plt.imshow(d,cmap='gray',vmin=1,vmax=255)
-# plt.show()
+# multiplicamos ambos miembros por izquierda para obtener una matriz
+# simetrica y aplicar cholesky
+Saux = (S.transpose()).dot(S)
+rEaux = (S.transpose()).dot(rE)
+he=solveEq(Saux,rEaux)
+
+z= np.zeros(M-L-1)
+Heaux=np.concatenate((he,z),axis=None)
+He= toeplitz(Heaux,np.zeros(M)) #con M He es de 512x512
+
+CN= (sigma**2)*np.eye(len(He))
+
+
+sigmax= (1/256*(sum( np.power (((range(0,255))-np.mean(range(0,255))),2))))**(1/2)
+mx= np.mean(range(0,255))*np.ones(M)
+CX= (sigmax**2)*np.eye(len(He))
+aux = (He.dot(CX)).dot(He.transpose())+CN
+invaux = np.linalg.inv(aux)
+mult1 = CX.dot(He.transpose())
+mult2 = mult1.dot(invaux)
+W = mult2
+# #W = CX.dot(He.transpose()).dot(np.linalg.inv((He.dot(CX)).dot(He.transpose)+CN))
+# # M = M+1
+# # P = P+1
+d = np.zeros((M,P))
+#mx es un array de 1x512
+example = (r[:, 0])-He.dot(mx)
+
+for k in range(0,P):
+    #d(:,k) accede en octave a la columna k-esima
+    d[:, k] = W.dot((r[:, k])-He.dot(mx)) + mx
+d = d.astype(int)
+
+plt.figure(2)
+plt.subplot(1,3,1)
+plt.imshow(a,cmap='gray',vmin=1,vmax=255)
+plt.subplot(1,3,2)
+plt.imshow(b,cmap='gray',vmin=1,vmax=255)
+plt.subplot(1,3,3)
+plt.imshow(d,cmap='gray',vmin=1,vmax=255)
+plt.show()
